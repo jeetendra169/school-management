0e5bc160f4dbbdc68d859411550f438f878d7a2e
prajapatidheerendra45 <Praja@123>
2025-02-13 14:09:02 +0530

    backend  auth setup

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-function db() {
-  return (
-    <div>
-      dsf
-    </div>
-  )
-}
+dotenv.config();
 
-export default db
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("✅ MongoDB Connected Successfully...!");
+    } catch (error) {
+        console.error("❌ MongoDB Connection Error:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
--- server.js
+++ server.js
@@ -1,4 +1,28 @@
-// eebddbfdbf
-// bhfjbadjbfbh
-// bjfdbdjbfkbdskb
-//hufdhundf
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+import path from 'path';
+import { fileURLToPath } from "url";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname,"./client/build")))
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
